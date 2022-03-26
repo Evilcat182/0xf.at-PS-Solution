1a5d8785a0079a6c8de573ed6f892dc22ec8a809
Evil182 <websuckz@web.de>
2022-03-26 15:55:11 +0100

    Level 18 added

---
+++ lvl18_morse-code.ps1
@@ -0,0 +1,52 @@
+﻿function ConvertFrom-MorseCode
+{
+    Param(
+        [string]$MorseCode
+    )
+
+    $mapping = @{
+        ".−" = 		"a"
+	    "−..." = 	"b"
+	    "−.−." = 	"c"
+	    "−.." = 	"d"
+	    "." = 		"e"
+	    "..−." = 	"f"
+	    "−−." = 	"g"
+	    "...." = 	"h"
+	    ".." = 		"i"
+	    ".−−−" = 	"j"
+	    "−.−" = 	"k"
+	    ".−.." = 	"l"
+	    "−−" = 		"m"
+	    "−." = 		"n"
+	    "−−−" = 	"o"
+	    ".−−." = 	"p"
+	    "−−.−" = 	"q"
+	    ".−." = 	"r"
+	    "..." = 	"s"
+	    "−" = 		"t"
+	    "..−" = 	"u"
+	    "...−" = 	"v"
+	    ".−−" = 	"w"
+	    "−..−" = 	"x"
+	    "−.−−" = 	"y"
+	    "−−.." =	"z"
+	    "−−−−−" = 	"0"
+	    ".−−−−" = 	"1"
+	    "..−−−" = 	"2"
+	    "...−−" = 	"3"
+	    "....−" = 	"4"
+	    "....." = 	"5"
+	    "−...." = 	"6"
+	    "−−..." = 	"7"
+	    "−−−.." = 	"8"
+	    "−−−−." = 	"9"
+    }
+
+    $arr = $MorseCode.Split(" ") | Where-Object {-Not [String]::IsNullOrEmpty($_)} 
+    $deCoded = ($arr | ForEach-Object {$mapping[$_]}) -join ''
+
+    $deCoded | Write-Output
+}
+
+ConvertFrom-MorseCode -MorseCode "...−− ....− −... ..−−− −.−. −.−. ...−− ..−. "
